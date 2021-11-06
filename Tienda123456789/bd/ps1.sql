22b6837f54092a50ae8a9e9245cbe370d9f40a8e
Jhon Mauricio Moreno García <jhonmorenogarcia@gmail.com>
2021-11-05 20:39:37 -0500

    Agrego fotos al index.

--- Tienda123456789/bd/ps1.sql
+++ Tienda123456789/bd/ps1.sql
@@ -44,3 +44,12 @@ ALTER TABLE ventas ADD CONSTRAINT FKventas885271 FOREIGN KEY (cb) REFERENCES pro
 ALTER TABLE empleados ADD CONSTRAINT FKempleados152462 FOREIGN KEY (documento) REFERENCES personas (documento);
 ALTER TABLE facturas ADD CONSTRAINT FKfacturas915308 FOREIGN KEY (empleadodocumento) REFERENCES empleados (documento);
 ALTER TABLE precios ADD CONSTRAINT FKprecios574748 FOREIGN KEY (cb) REFERENCES productos (cb);
+
+
+CREATE TABLE fotos (
+  id          int(11) NOT NULL AUTO_INCREMENT, 
+  descripcion varchar(255) NOT NULL, 
+  foto        varchar(255) NOT NULL, 
+  productoscb varchar(25) NOT NULL, 
+  PRIMARY KEY (id)) ENGINE=InnoDB;
+ALTER TABLE fotos ADD CONSTRAINT FKfotos946444 FOREIGN KEY (productoscb) REFERENCES productos (cb);
